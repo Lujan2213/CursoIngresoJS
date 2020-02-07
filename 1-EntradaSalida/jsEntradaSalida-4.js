651024e92866a79b9f7f696dea5dd5958cc62d61
Lujan2213 <60760627+Lujan2213@users.noreply.github.com>
2020-02-06 22:16:44 -0300

    Add files via upload
    Ejercisios clase 1

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,12 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+{ var nombre;
+	nombre=prompt("ingrese su nombre");
+	document.getElementById("elNombre").value = nombre;
+	
+
+
 
 }
 
