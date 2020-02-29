04f39d8e9afb041716ae04fa20afd273edde69db
Lujan2213 <60760627+Lujan2213@users.noreply.github.com>
2020-02-29 00:58:34 -0300

    Lujan

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
-
+var marca;
+var peso =0;
+var temperatura =0;
+var cantidadProductos = 0;
+var cantidadTemperatura;
+var cantidadProductosConservan =0;
+var promedioPeso =0;
+var pesoMax=0;
+var pesoMin;
 }
